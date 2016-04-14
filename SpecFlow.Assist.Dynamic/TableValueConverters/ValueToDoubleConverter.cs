69d91c3327f990c5cd61f088efc14bc8c30a39b6
Vedran Zakanj <vedran@mono.hr>
2016-04-14 12:55:21 +0200

    Add additional custom value converters.

---
+++ SpecFlow.Assist.Dynamic/TableValueConverters/ValueToDoubleConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpecFlow.Assist.Dynamic.TableValueConverters
+{
+    /// <summary>
+    /// Tries to convert the value to <see cref="double"/>, otherwise returns null.
+    /// </summary>
+    public class ValueToDecimalConverter : ITableValueConverter
+    {
+        public object Convert(string tableValue)
+        {
+            double value;
+            if (Double.TryParse(tableValue, out value))
+            {
+                return value;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ SpecFlow.Assist.Dynamic/TableValueConverters/ValueToIntConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpecFlow.Assist.Dynamic
+{
+    /// <summary>
+    /// Tries to convert the value to int, otherwise returns null.
+    /// </summary>
+    public class ValueToIntConverter : ITableValueConverter
+    {
+        public object Convert(string tableValue)
+        {
+            int value;
+            if (int.TryParse(tableValue, out value))
+            {
+                return value;
+            }
+
+            return null;
+        }
+    }
+}
