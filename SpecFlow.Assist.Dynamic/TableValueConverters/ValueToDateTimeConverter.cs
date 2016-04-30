05af4d26914eb7aabadacc9ee0c5426a81e7e6f3
Vedran Zakanj <vedran.zakanj@gmail.com>
2016-04-30 15:05:09 +0200

    Add stub implementation of value to DateTime converter.

---
+++ SpecFlow.Assist.Dynamic/TableValueConverters/ValueToDateTimeConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpecFlow.Assist.Dynamic
+{
+    public class ValueToDateTimeConverter : ITableValueConverter
+    {
+        public object Convert(string tableValue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
